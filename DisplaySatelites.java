ced5b3a45988e50697459ef1a2641997467f4cbf
Jitesh Bhakat <137819786+Jitesh8260@users.noreply.github.com>
2023-12-22 22:28:01 +0530

    Update DisplaySatelites.java

--- DisplaySatelites.java
+++ DisplaySatelites.java
@@ -1,3 +1,16 @@
+/*Suppose we want to represent a planet in the solar system as an object of class Planet. In
+the object, it contains two fields: one called name is a String containing its name, and one
+called satellites is an array of String containing the names of all its satellites. Define the
+class, with a constructor taking a String and an array of String as argument for initializing
+the object.
+Write a method print() in the class to print out the name of the planet and all the satellites.
+For example, if we have
+String[] earth_sat = { "Moon" };
+Planet earth = new Planet("Earth", earth_sat);
+Then we want earth.print(); to print out the following:
+Earth has 1 satellite(s):
+Moon.*/
+
 import java.util.Scanner;
 
 class DisplaySatelites{
